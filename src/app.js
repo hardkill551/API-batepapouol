40fb2c1d6bef2f62b22139f297927b1a36c56459
hardkill551 <hardkill194@gmail.com>
2023-04-14 15:58:00 -0300

    post status feito

--- src/app.js
+++ src/app.js
@@ -113,6 +113,19 @@ app.get("/messages", async(req, res)=>{
 
 })
 
+app.post("/status", async(req, res)=>{
+    const {user} = req.headers
+    if(!user) return res.sendStatus(404)
+    try{
+        const participant = await db.collection("participants").findOne({name: user})
+        if(!participant) return res.sendStatus(404)
+        await db.collection("participants").updateOne({name: user},{$set: {lastStatus: Date.now()}})
+        res.sendStatus(200)
+    }   catch(err){
+
+    }
+})
+
 
 
 
